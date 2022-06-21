df3afc3288a5b283cc82db20ad5a17ca12d52ee3
Alderi Araújo <104461260+AlderiAraujo@users.noreply.github.com>
2022-06-21 16:51:12 -0300

    Update Tabuleiro.py

--- Modulo/Tabuleiro.py
+++ Modulo/Tabuleiro.py
@@ -1,16 +1,19 @@
 import pygame
-
-matriz = []
-
+#variáveis usadas para a criação do tabuleiro
+tabuleiro = []
 linha = 8
 coluna = 8
-
 AZUL = (0, 0, 255)
-
+#loop para criação da matriz
 for l in range(0, linha):
     listaLinha = []
     for c in range(0, coluna):
         listaLinha.append(0)
-    matriz.append(listaLinha)
+    tabuleiro.append(listaLinha)
 
-
+#impressão da matriz do tabuleiro
+print(f'\tA \tB \tC \tD \tE \tF \tG \tH')
+IDLinhaInicial = 1
+for pos,item in enumerate(tabuleiro):
+    IDLinha = IDLinhaInicial + pos
+    print(IDLinha, item)
