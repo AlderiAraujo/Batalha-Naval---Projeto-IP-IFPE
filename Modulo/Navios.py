869d68ae9df41fb7285228aa03c57aa9a2bcc755
Alderi Araújo <104461260+AlderiAraujo@users.noreply.github.com>
2022-06-21 22:44:13 -0300

    Create Navios.py

---
+++ Modulo/Navios.py
@@ -0,0 +1,8 @@
+import pygame
+
+sub = "S"
+pos_sub = 1
+quant_sub = 3
+
+img_sub = pygame.image.load("C:/Users/Alderi/Documents/GitHub/Batalha-Naval---Projeto-IP-IFPE/Modulo/submarino.png")
+img_subTabuleiro = pygame.transform.scale(img_sub, (30,30))
