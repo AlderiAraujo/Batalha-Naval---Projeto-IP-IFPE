18b6fd66c1bea34a465d0434ad7ce5e16f5eb9e1
Alderi Araújo <104461260+AlderiAraujo@users.noreply.github.com>
2022-06-25 02:57:52 -0300

    Update Tabuleiro.py

--- Modulo/Tabuleiro.py
+++ Modulo/Tabuleiro.py
@@ -2,113 +2,90 @@ import pygame
 #variáveis usadas para a criação do tabuleiro
 linha = 8
 coluna = 8
+RED = (255, 0, 0)
 
-tabuleiro1 = []
-indice_alfa1 = []
-indice_num1 = []
+tabuleiro = []
+IndiceAlfa = []
+IndiceNum = []
 
-tabuleiro2 = []
-indice_alfa2 = []
-indice_num2 = []
+tela = pygame.display.set_mode((1060,480))  #dimensões do tabuleiro na janela
 
-tela = pygame.display.set_mode((1060,480))
-img_oceano = pygame.image.load("./Repositorio-Imagens/oceano.jpg")
-img_oceanoTabuleiro = pygame.transform.scale(img_oceano, (48,48))
-imgA = pygame.image.load("./Repositorio-Imagens/A.jpg")
-imgATabuleiro = pygame.transform.scale(imgA, (48,48))
-imgB = pygame.image.load("./Repositorio-Imagens/B.jpg")
-imgBTabuleiro = pygame.transform.scale(imgB, (48,48))
-imgC = pygame.image.load("./Repositorio-Imagens/C.jpg")
-imgCTabuleiro = pygame.transform.scale(imgC, (48,48))
-imgD = pygame.image.load("./Repositorio-Imagens/D.jpg")
-imgDTabuleiro = pygame.transform.scale(imgD, (48,48))
-imgE = pygame.image.load("./Repositorio-Imagens/E.jpg")
-imgETabuleiro = pygame.transform.scale(imgE, (48,48))
-imgF = pygame.image.load("./Repositorio-Imagens/F.jpg")
-imgFTabuleiro = pygame.transform.scale(imgF, (48,48))
-imgG = pygame.image.load("./Repositorio-Imagens/G.jpg")
-imgGTabuleiro = pygame.transform.scale(imgG, (48,48))
-imgH = pygame.image.load("./Repositorio-Imagens/H.jpg")
-imgHTabuleiro = pygame.transform.scale(imgH, (48,48))
-img = pygame.image.load("./Repositorio-Imagens/H.jpg")
-img_Tabuleiro = pygame.transform.scale(imgH, (48,48))
-
-
-
-
-
-for l in range(0, linha):
-    indice_alfa1.append(imgATabuleiro)
-
-for l in range(0, linha):
-    indice_num1.append(img_Tabuleiro)
-
-#loop para criação da matriz
+#load e redimensionamento de imagem de fundo e loop para criação de matriz para o tabuleiro
+ImgOceano = pygame.image.load("./Repositorio-Imagens/oceano.jpg")
+ImgOceanoTabuleiro = pygame.transform.scale(ImgOceano, (48,48))
 for l in range(0, linha):
     listaLinha = []
     for c in range(0, coluna):
-        listaLinha.append(img_oceanoTabuleiro)
-    tabuleiro1.append(listaLinha)
+        listaLinha.append(ImgOceanoTabuleiro)
+    tabuleiro.append(listaLinha)
 
-for l in range(0, linha):
-    indice_alfa2.append(img_Tabuleiro)
+#loop para load, redimensionamento e adição de imagens dos índices alfabeticos(colunas) em lista
+base = 65
+for cont in range (0, linha):
+    atual = base + cont
+    Img = pygame.image.load(f"./Repositorio-Imagens/Img{atual}.jpg")
+    TabuleiroImg = pygame.transform.scale(Img, (48, 48))
+    IndiceAlfa.append(TabuleiroImg)
+#loop para load, redimensionamento e adição de imagens dos índices numericos(linhas) em lista
+base = 1
+for cont in range (0, linha):
+    atual = base + cont
+    Img = pygame.image.load(f"./Repositorio-Imagens/Img{atual}.jpg")
+    TabuleiroImg = pygame.transform.scale(Img, (48, 48))
+    IndiceNum.append(TabuleiroImg)
 
-for l in range(0, linha):
-    indice_num2.append(img_Tabuleiro)
-
-for l in range(0, linha):
-    listaLinha = []
-    for c in range(0, coluna):
-        listaLinha.append(img_oceanoTabuleiro)
-    tabuleiro2.append(listaLinha)
-
-sair = True
-while sair:
+#loop para geração de eventos
+continuar = True
+while continuar:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:       #condição para saída, apertando o botão "X" da janela
-            sair = False
+        if event.type == pygame.QUIT:   #condição para saída do loop de eventos, apertando o botão "X" da janela
+            continuar = False
+    pygame.draw.rect(tela,RED, [525,0,10,480])  #linha divisória dos tabuleiros
 
+#loops para posicionamento das imagens dos tabuleiros na janela
+#loop para posicionamento dos índices alfabeticos(colunas) do tabuleiro 1
     pos_y = 0
     for l in range(0, linha):
         pos_x = 50
         for c in range(0, coluna):
-            tela.blit(indice_alfa1[l], (pos_x, pos_y))
+            tela.blit(IndiceAlfa[c], (pos_x, pos_y))
             pos_x += 50
-
+#loop para posicionamento dos índices numéricos(linhas) do tabuleiro 1
     pos_y = 50
     for l in range(0, linha):
         pos_x = 0
         for c in range(0, coluna):
-            tela.blit(indice_num1[c], (pos_x, pos_y))
+            tela.blit(IndiceNum[l], (pos_x, pos_y))
         pos_y += 50
-
+#loop para posicionamento da matriz de fundo do tabuleiro 1
     pos_y = 50
     for l in range(0, linha):
         pos_x = 50
         for c in range(0, coluna):
-            tela.blit(tabuleiro1[l][c], (pos_x, pos_y))
-            pos_x+= 50
-        pos_y+=50
+            tela.blit(tabuleiro[l][c], (pos_x, pos_y))
+            pos_x += 50
+        pos_y += 50
 
+#loop para posicionamento dos índices alfabeticos(colunas) do tabuleiro 2
     pos_y = 0
     for l in range(0, linha):
         pos_x = 610
         for c in range(0, coluna):
-            tela.blit(indice_alfa2[l], (pos_x, pos_y))
+            tela.blit(IndiceAlfa[c], (pos_x, pos_y))
             pos_x += 50
-
+#loop para posicionamento dos índices numéricos(linhas) do tabuleiro 2
     pos_y = 50
     for l in range(0, linha):
         pos_x = 1010
         for c in range(0, coluna):
-            tela.blit(indice_num2[c], (pos_x, pos_y))
+            tela.blit(IndiceNum[l], (pos_x, pos_y))
         pos_y += 50
-
+#loop para posicionamento da matriz de fundo do tabuleiro 2
     pos_y = 50
     for l in range(0, linha):
         pos_x = 610
         for c in range(0, coluna):
-            tela.blit(tabuleiro2[l][c], (pos_x, pos_y))
+            tela.blit(tabuleiro[l][c], (pos_x, pos_y))
             pos_x += 50
         pos_y += 50
 
