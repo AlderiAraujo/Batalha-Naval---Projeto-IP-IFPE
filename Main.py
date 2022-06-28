f8e8cdf8b48727efb6a33282a3b431561ef190d5
Alderi Araújo <104461260+AlderiAraujo@users.noreply.github.com>
2022-06-28 02:10:49 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,23 @@
 import pygame
-from pygame.locals import *
+from Modulo.Tabuleiro import FundoTabuleiro, TabuleiroPlayer1, TabuleiroPlayer2
+from Modulo.Jogabilidade import InserirBarco
 
+tela = pygame.display.set_mode((1060,500))
+pygame.display.set_caption("Batalha Naval")
+RED = (255, 0, 0)
+
+continuar = True
+while continuar:
+    for event in pygame.event.get():
+        pygame.draw.rect(tela, RED, [525, 0, 10, 500])  # linha divisória dos tabuleiros
+        if event.type == pygame.QUIT:   #condição para saída do loop de eventos, apertando o botão "X" da janela
+            continuar = False
+    #funções de tabuleiro
+        FundoTabuleiro()
+        TabuleiroPlayer1()
+        TabuleiroPlayer2()
+    #funções de jogabilidade
+
+
+
+    pygame.display.update() #comando para atualização de frame
