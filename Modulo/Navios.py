5ff2cb0f71e9a58d1c2d226a360b856a6a30a318
Alderi Araújo <104461260+AlderiAraujo@users.noreply.github.com>
2022-06-28 02:10:02 -0300

    Update Navios.py

--- Modulo/Navios.py
+++ Modulo/Navios.py
@@ -1,8 +1,11 @@
 import pygame
 
-Submarino = "S"
 PosSubmarino = 1
 QuantSubmarino = 3
 
 ImgSubmarino = pygame.image.load("./Repositorio-Imagens/submarino.png")
 TabuleiroImgSubmarino = pygame.transform.scale(ImgSubmarino, (30,30))
+
+def Submarino ():
+    print(f"Navegação de ocupação de {PosSubmarino} casa, podendo posicionar até {QuantSubmarino} no tabuleiro.")
+    return TabuleiroImgSubmarino
