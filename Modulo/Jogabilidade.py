532068584ca20e5b93263dd68b2b06b9aa2f8e06
Arthur Pedrosa <91102862+Arthurpdrs@users.noreply.github.com>
2022-07-02 18:40:57 -0300

    arquivo teste
    Cria um arquivo para testes e adiciona função em Jogabilidade

--- Modulo/Jogabilidade.py
+++ Modulo/Jogabilidade.py
@@ -1,6 +1,18 @@
 import pygame
+from pygame import event
 
-def InserirBarco ():
+
+
+def InserirBarco():
     if event.type == pygame.MOUSEBUTTONDOWN:
         X = pygame.mouse.get_pos()[0]
         Y = pygame.mouse.get_pos()[1]
+
+def verifica_clique():
+    posicao_mouse = pygame.mouse.get_pos()
+    posX_mouse, posY_mouse = posicao_mouse
+
+    print(posicao_mouse)
+    print(posX_mouse, posY_mouse)
+    return posicao_mouse
+
--- Teste.py
+++ Teste.py
@@ -1,10 +1,14 @@
+#Arquivo cópia do Main para efeito de teste, deve ser apagado no fim do projeto
 import pygame
-from Modulo.Tabuleiro import FundoTabuleiro, TabuleiroPlayer1, TabuleiroPlayer2
+from Modulo.Tabuleiro import *
 from Modulo.Jogabilidade import *
+
 
 tela = pygame.display.set_mode((1060,500))
 pygame.display.set_caption("Batalha Naval")
 RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+
 
 continuar = True
 while continuar:
@@ -12,11 +16,23 @@ while continuar:
         pygame.draw.rect(tela, RED, [525, 0, 10, 500])  # linha divisória dos tabuleiros
         if event.type == pygame.QUIT:   #condição para saída do loop de eventos, apertando o botão "X" da janela
             continuar = False
-    #funções de tabuleiro
+        #funções de tabuleiro
+
         FundoTabuleiro()
         TabuleiroPlayer1()
         TabuleiroPlayer2()
+
+        rect = pygame.draw.rect(tela, WHITE, (450, 450, 48, 48))
+        tela.blit(ImgOceanoTabuleiro, (450, 450))
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if rect.collidepoint(pygame.mouse.get_pos()):
+                print("clicou dentro")
+            else:
+                print("clicou fora")
+
     #funções de jogabilidade
+
 
 
 
