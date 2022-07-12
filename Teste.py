ebbd1918c62ba47fd74c5b890f871a8ee2b39b2c
Alderi Araújo <104461260+AlderiAraujo@users.noreply.github.com>
2022-07-11 23:31:59 -0300

    Update Teste.py

--- Teste.py
+++ Teste.py
@@ -1,8 +1,8 @@
 #Arquivo cópia do Main para efeito de teste, deve ser apagado no fim do projeto
 import pygame
-from modulo.TabuleiroTest import *
-from modulo.Jogabilidade import Jogabilidade
-
+from modulo.tabuleiroTest import *
+from modulo.jogabilidade import Jogabilidade
+from modulo.menu import menu
 
 tela = pygame.display.set_mode((1060,500))
 pygame.display.set_caption("Batalha Naval")
@@ -19,16 +19,23 @@ while continuar:
         if event.type == pygame.QUIT:   #condição para saída do loop de eventos, apertando o botão "X" da janela
             continuar = False
 
+        #menu(tela)
+
         #funções de tabuleiro
-        tabuleiroJogador = Tabuleiro(tela, 50, 50)
+        tabuleiroJogador1 = Tabuleiro(tela, 50, 50)
         tabuleiroJogador2 = Tabuleiro(tela, 610, 50)
 
         #funções de jogabilidade
         if event.type == pygame.MOUSEBUTTONDOWN:
             joga = Jogabilidade()
-            joga.avaliaCliqueTabuleiro(tabuleiroJogador.matrizTabuleiro, 50, 50)
+            joga.avaliaCliqueTabuleiro(tabuleiroJogador1.matrizTabuleiro, 50, 50)
             print(joga.pos_tela)
             print(joga.pos_clicada)
+
+            joga.avaliaCliqueTabuleiro(tabuleiroJogador2.matrizTabuleiro, 610, 50)
+            print(joga.pos_tela)
+            print(joga.pos_clicada)
+
 
 
 
