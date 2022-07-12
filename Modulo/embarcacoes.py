b8dca051a28acd3ec2499e7134ea99d1e585aad4
Alderi Araújo <104461260+AlderiAraujo@users.noreply.github.com>
2022-07-11 23:31:02 -0300

    Update embarcacoes.py

--- Modulo/embarcacoes.py
+++ Modulo/embarcacoes.py
@@ -1,24 +1,39 @@
 import pygame
 
+altura = largura = 48
 class Embarcacao:
 
-    def __init__(self, nome, imagem):
+    def __init__(self, nome, imagem, quantidade, pontuacao):
 
         self.nome = nome
         self.imagem = imagem
+        self.quantidade = quantidade
+        self.pontuacao = pontuacao
 
         if self.nome == "submarino":
+            self.ImgSubmarino = pygame.image.load("./Repositorio-Imagens/submarino.png")
+            self.ImgSubmarinoFormat = pygame.transform.scale(self.ImgSubmarino, (largura, altura))
+            self.quantidade = 5
             self.pontuacao = 1
 
-        elif self.nome == "portaAvioes":
-            self.pontuacao = 2
 
         elif self.nome == "cruzador":
+            self.ImgCruzador = pygame.image.load("./Repositorio-Imagens/cruzador.png")
+            self.ImgCruzadorFormat = pygame.transform.scale(self.ImgCruzador, (largura, altura))
+            self.quantidade = 3
+            self.pontuacao = 2
+
+
+        elif self.nome == "portaAvioes":
+            self.ImgPortaAvioes = pygame.image.load("./Repositorio-Imagens/portaAvioes.png")
+            self.ImgPortaAvioesFormat = pygame.transform.scale(self.ImgPortaAvioes, (largura, altura))
+            self.quantidade = 1
             self.pontuacao = 4
 
 
-        #ImgSubmarino = pygame.image.load("./Repositorio-Imagens/submarino.png")
-        #TabuleiroImgSubmarino = pygame.transform.scale(ImgSubmarino, (30,30))
+
+
+
 
 
 
